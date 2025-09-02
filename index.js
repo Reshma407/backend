a2996d62c29df4293d5cccca99efad0d4e59599e
Reshma407 <reshmabasnet088@gmail.com>
2025-09-02 10:22:10 +0545

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello");
+});
+
+app.post("/", (req, res) => {
+    res.send("Server is running");
+});
+
+app.listen(8000, () => {
+    console.log("Server is running on port 8000");
+});
