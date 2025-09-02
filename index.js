9eba7a54b86facb55d3a3b696b84ab88fa1e8bf9
Reshma407 <reshmabasnet088@gmail.com>
2025-09-02 10:36:58 +0545

    Add basic Express server

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello");
+  res.send("Server is running");
 });
 
-app.post("/", (req, res) => {
-    res.send("Server is running");
+app.get("/about", (req, res) => {
+  res.send("This is about page");
 });
 
 app.listen(8000, () => {
-    console.log("Server is running on port 8000");
+  console.log("server is running on port 8000");
 });
